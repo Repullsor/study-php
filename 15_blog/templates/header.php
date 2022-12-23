fef51037f9bf07084c92d220301209b252d4a226
Repullsor <rafaelvb_@hotmail.com>
2022-12-23 15:59:55 -0300

    Conclusão do projeto Blog

--- 15_blog/templates/header.php
+++ 15_blog/templates/header.php
@@ -1,5 +1,7 @@
 <?php
   include_once("helpers/url.php");
+  include_once("data/posts.php");
+  include_once("data/categories.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Codar</title>
     <!-- ESTILOS DO PROJETO -->
-    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
     <!-- GOOGLE FONTS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
@@ -21,10 +23,10 @@
         </a>
         <nav>
             <ul id="navbar">
-                <li><a href="<?= $BASE_URL ?>">Home</a></li>
-                <li><a href="#">Categorias</a></li>
-                <li><a href="#">Sobre</a></li>
-                <li><a href="<?= $BASE_URL ?>contato.php">Contato</a></li>
+                <li><a href="<?= $BASE_URL ?>" class="nav-link">Home</a></li>
+                <li><a href="#" class="nav-link">Categorias</a></li>
+                <li><a href="#" class="nav-link">Sobre</a></li>
+                <li><a href="<?= $BASE_URL ?>contato.php" class="nav-link">Contato</a></li>
             </ul>
         </nav>
     </header>
